d965f8ee1e012bed7288c8c590104d795873b78d
league <league@imac-38.attlocal.net>
2020-03-09 18:24:43 -0700

    adventures of berginstisington the great

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -24,15 +24,16 @@ public class DynamicIntegerArray {
 		//   to the new array
 		
 		for (int i = 0; i < numbing.length; i++) {
-			numbing[i] += stories[i];
+			 stories[i] = numbing[i];
 		}
 		//D. set the member array equal to the new array.
+		numbing = stories ;
 	}
 	
 	//4. Complete the steps in the get method
 	public int get(int location) {
 		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		return 	numbing[location];
 	}
 	
 	//5. Run the DynamicArrayTest to see if you are correct so far.
@@ -42,15 +43,28 @@ public class DynamicIntegerArray {
 	public void set(int v, int location) {
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
+		
+		numbing[location] = v;
+		
 	}
 	
 	//7. Complete the steps in the insert method
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] bobbys = new int[numbing.length+1];
 		//B. Make a for loop that iterates through the new array
-
+for (int i = 0; i < bobbys.length; i++) {
+	if (i < location) {
+		bobbys[i] = numbing[i];
+	}
+	else if (i > location) {
+		bobbys[i] = numbing[i-1];
+	}
+	else {
+		bobbys[i] = v;
+	}
+}
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
 			
@@ -61,7 +75,7 @@ public class DynamicIntegerArray {
 		
 		
 		//F. set the member array equal to the new array
-	
+numbing = bobbys ;
 	}
 	
 	//8. Run the tests again and check your progress
@@ -69,9 +83,19 @@ public class DynamicIntegerArray {
 	//9. Complete the steps in the remove method
 	public void remove(int location) {
 		//A. create a new array that is one element smaller than the member array
-		
+		int[] googles = new int[numbing.length+1];
 		//B. make a for loop to iterate through the member array
-		
+		for (int i = 0; i < numbing.length; i++) {
+			if (i < location) {
+				googles[i] = numbing[i];
+			}
+			else if (i > location) {
+				googles[i-1] = numbing[i];
+			}
+			else {
+				continue;
+				}
+		}
 			//C. if i  is less than location
 			//		set the element at i of the new array to the element at i of the member array
 			
@@ -81,18 +105,23 @@ public class DynamicIntegerArray {
 			//E. else, continue;
 			
 		//F. set the member array equal to the new array
+		numbing = googles;
 	}
 	
 	//10. Run the tests again the see if you are correct so far
 	
 	//11. Complete the size method so that it returns the length of the member array.
 	public int size() {
-		return 0;
+		
+		return numbing.length;
 	}
 	
 	//12. Complete the clear array so that it sets the member array 
 	//    equal to a new integer array of size 0
 	public void clear() {
+		int[] berginstien = new int[0];
+		numbing = berginstien ;
+		
 	}
 	
 	//13. Run the test again to see if you are finished.
--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,36 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+ArrayList<String> berginstiensintons = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+berginstiensintons.add("berg");
+berginstiensintons.add("bergin");
+berginstiensintons.add("berginstien");
+berginstiensintons.add("berginstiensinton");
+berginstiensintons.add("berginstiensintons");
 		//3. Print all the Strings using a standard for-loop
-		
+for (int j = 0; j < berginstiensintons.size(); j++) {
+	
+		System.out.println(berginstiensintons.get(j));
+}
 		
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < berginstiensintons.size(); i+=2) {
+			System.out.println(berginstiensintons.get(i));
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = berginstiensintons.size(); i >= 0; i--) {
+			System.out.println(berginstiensintons.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int j = 0; j < berginstiensintons.size(); j++) {
+			if (berginstiensintons.get(j).contains("e")) {
+				System.out.println(berginstiensintons.get(j));
+			}
+		//	System.out.println(berginstiensintons.get(j));
+	}
+		
 	}
 }
