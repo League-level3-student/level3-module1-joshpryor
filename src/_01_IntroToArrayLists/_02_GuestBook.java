1f2934b6902723ad95d49dbab16201dece3559a5
league <league@imac-38.attlocal.net>
2020-03-09 19:31:48 -0700

    guest book and crows ip

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,7 +1,24 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	ArrayList <String> franktonstienithon = new ArrayList<String>();
+	public static void main(String[] args) {
+		_02_GuestBook bobithanington = new _02_GuestBook();
+		bobithanington.code();
+	
+	}
+	JButton addd = new JButton();
+	JButton vew = new JButton();
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,7 +26,37 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-	
+	void code() {
+		JFrame window = new JFrame("Guest_Book");
+		JPanel panal = new JPanel();
+
+		addd.setText("Add Name");
+		
+		vew.setText("View Name");
+		window.add(panal);
+		panal.add(vew);
+		panal.add(addd);
+		window.setVisible(true);
+		window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+		window.pack();
+		addd.addActionListener(this);
+		vew.addActionListener(this);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if ( addd == e.getSource() ) {
+			String add = JOptionPane.showInputDialog(" Enter a name. ");
+			franktonstienithon.add(add);
+		}
+		if ( vew == e.getSource() ) {
+			for (int i = 0; i < franktonstienithon.size(); i++) {
+				JOptionPane.showMessageDialog(null, "Guest #" + franktonstienithon.get(i) + ":  " + vew);		
+			}
+		}
+	}
 	
 	
 }
